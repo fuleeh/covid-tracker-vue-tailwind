07ffe7cc9519be6887a57985a9b5838d9cacd336
Fülöp Szilárd <fszili882@gmail.com>
2022-04-10 02:00:15 +0300

    covid tracker with covid19api

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,29 @@
+<template>
+  <select
+    v-model="selected"
+    class="form-select block w-full border p-3 rounded mt-10"
+    @change="onChange"
+  >
+    <option value="0">Select Country</option>
+    <option v-for="country in countries" :key="country.ID">
+      {{ country.Country }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  props: ['countries'],
+  data() {
+    return { selected: 0 };
+  },
+  methods: {
+    onChange() {
+      const country = this.countries.find(
+        (item) => item.Country === this.selected
+      );
+      this.$emit('get-country', country);
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main v-if="!isLoading">
+    <data-title :text="title" :dataDate="dataDate" />
+    <data-boxes :stats="stats" />
+    <country-select @get-country="getCountryData" :countries="countries" />
+    <button
+      @click="clearCountryData"
+      v-if="stats.Country"
+      class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600"
+    >
+      Clear Country
+    </button>
+  </main>
+  <main class="flex flex-col align-center justifiy-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
+    <img :src="loadingImage" class="w-24 m-auto" />
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import DataTitle from '../components/DataTitle.vue';
+import DataBoxes from '../components/DataBoxes.vue';
+import CountrySelect from '../components/CountrySelect.vue';
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  components: { DataTitle, DataBoxes, CountrySelect },
+  data() {
+    return {
+      isLoading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+      loadingImage: require('../assets/hourglass.gif'),
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      const response = await fetch('https://api.covid19api.com/summary');
+      const data = await response.json();
+
+      return data;
+    },
+    getCountryData(country) {
+      this.stats = country;
+      this.title = country.Country;
+    },
+    async clearCountryData() {
+      this.isLoading = true;
+      const data = await this.fetchCovidData();
+      this.title = 'Global';
+      this.stats = data.Global;
+      this.isLoading = false;
+    },
+  },
+
+  async created() {
+    const data = await this.fetchCovidData();
+
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.isLoading = false;
+  },
+};
 </script>
